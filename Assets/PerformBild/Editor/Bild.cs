ace3f0507148c41eaecd519453a9ec1811ab6b4e
Ignas Siaudkulis <Ignassi@unity3d.com>
2017-10-25 16:34:24 +0300

    Test
    a

---
+++ Assets/PerformBild/Editor/Bild.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Build : Editor
+{
+    [MenuItem("Bild/Bild")]
+    public static void PerformTestBuild()
+    {
+        string[] scenes = { "Assets/Scenes/InitialScene.unity"};
+        string path = string.Format("F:/Build/{0}", DateTime.Now.ToString("yyyyMMMddHHmm"));
+        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+        using (TextWriter writer = File.CreateText(path + "/buildlog.txt"))
+        {
+            writer.WriteLine("Start buid...");
+            writer.Write(BuildPipeline.BuildPlayer(scenes, path + "/build.exe", BuildTarget.StandaloneWindows64, BuildOptions.None));
+            writer.WriteLine("Finished!");
+        }
+    }
+
+}
